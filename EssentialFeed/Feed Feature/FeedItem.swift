84f5449ac4e0a48333810e32bc81d16b4e5e22db
Charita <charita.dec@gmail.com>
2023-08-17 17:42:16 +0530

    Initial commit

---
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Charita R on 17/08/23.
+//
+
+import Foundation
+
+
+struct FeedItem {
+    let id: UUID
+    let location: String?
+    let description: String?
+    let imageURL: URL
+}
---
+++ EssentialFeed/Feed Feature/FeedLoader.swift
@@ -0,0 +1,17 @@
+//
+//  FeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Charita R on 17/08/23.
+//
+
+import Foundation
+
+enum FeedLoaderResult {
+    case succes([FeedItem])
+    case error(Error)
+}
+
+protocol FeedLoader {
+    func load(completion: (FeedLoaderResult) -> Void)
+}
